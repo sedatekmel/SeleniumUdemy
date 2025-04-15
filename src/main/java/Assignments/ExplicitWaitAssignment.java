9283a0dc6dcafce4683cdfab95ff99b14d92ff28
sedatekmel <sedatekmel_91@hotmail.com>
2025-04-15 14:23:21 +0300

    Exercises

--- src/main/java/Assignments/ExplicitWaitAssignment.java
+++ src/main/java/Assignments/ExplicitWaitAssignment.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class ExplicitWaitAssignment {
     public static void main(String[] args) throws InterruptedException {
@@ -16,13 +17,16 @@ public class ExplicitWaitAssignment {
         String password="learning";
         System.setProperty("webdriver.chrome.driver", "/Users/sedatekmel/Desktop/web drivers/chromedriver");
         WebDriver driver=new ChromeDriver();
+        WebDriverWait w =new WebDriverWait(driver, Duration.ofSeconds(3));
         driver.get("https://rahulshettyacademy.com/loginpagePractise/");
-        signIn(driver,username,password);
-        addItemsToBasket(driver);
+        driver.manage().window().maximize();
+        signIn(driver,username,password,w);
+        addItemsToBasket(driver,w);
+        driver.close();
     }
 
-    public static void signIn(WebDriver driver,String username, String password) throws InterruptedException {
-        WebDriverWait w =new WebDriverWait(driver, Duration.ofSeconds(3));
+    public static void signIn(WebDriver driver,String username, String password,WebDriverWait w) throws InterruptedException {
+
         driver.findElement(By.id("username")).sendKeys(username);
         driver.findElement(By.id("password")).sendKeys(password);
         driver.findElement(By.xpath("(//span[@class='checkmark'])[2]")).click(); // alternatif yöntem bak
@@ -35,10 +39,18 @@ public class ExplicitWaitAssignment {
         driver.findElement(By.id("signInBtn")).click();
     }
 
-    public static void addItemsToBasket(WebDriver driver)
+    spublic static void addItemsToBasket(WebDriver driver,WebDriverWait w)
     {
         //Add butonu tıklanabilir olana kadar signInBtn sonrası beklemeye al.
-
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText("Checkout")));  Alternatif Shutty Çözümü
+        w.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='btn btn-info']")));
+        List<WebElement> addItemButtons=driver.findElements(By.xpath("//button[@class='btn btn-info']"));
+        for (WebElement addItemButton : addItemButtons)
+        {
+            addItemButton.click();
+        }
+        int addedItemCount=addItemButtons.size();
+        driver.findElement(By.xpath("//a[@class='nav-link btn btn-primary']")).click();
     }
 
 }
