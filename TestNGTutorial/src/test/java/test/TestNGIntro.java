1375ed8ca2fb23c76d5aa6e23a5daf2b2c41b027
sedatekmel <sedatekmel_91@hotmail.com>
2025-05-28 20:30:07 +0300

    ex

---
+++ TestNGTutorial/src/test/java/test/TestNGIntro.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class TestNGIntro {
+
+    @Test
+    public void sayHello()
+    {
+        System.out.println("Hello");
+    }
+
+    @Test
+    public void sayGoodBye()
+    {
+        System.out.println("Good Bye");
+    }
+
+}
+
